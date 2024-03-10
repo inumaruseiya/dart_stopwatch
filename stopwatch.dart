0beb8a9a2f0ba517ed288e55df2c595e5fbd7b36
inumaruseiya <s.inumaru2023@gmail.com>
2024-03-10 17:50:07 +0900

    ストップウォッチ作成

--- stopwatch.dart
+++ stopwatch.dart
@@ -4,10 +4,9 @@ void main() {
   final stopwatch = Stopwatch()..start();
   print('ストップウォッチを停止するにはEnterを入力してください。');
   stdin.listen((data) {
-    // キー入力があった場合
     stopwatch.stop();
-    print('\nストップウォッチを停止しました。経過時間: ${stopwatch.elapsed}');
-    exit(0); // プログラムを終了
+    print('ストップウォッチを停止しました。経過時間: ${stopwatch.elapsed}');
+    exit(0); 
   });
 }
 
